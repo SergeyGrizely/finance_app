338e61f1a6c5b1ebc80cecf2519a78bafd9e101a
SergeyGrizely <www.sb4133@gmail.com>
2026-02-13 12:02:18 +0300

    add mult_lang

--- backend/app/database.py
+++ backend/app/database.py
@@ -9,7 +9,14 @@ if "postgresql://" in db_url:
 elif "postgres://" in db_url:
     db_url = db_url.replace("postgres://", "postgresql+psycopg2://")
 
-engine = create_engine(db_url, echo=False)
+engine = create_engine(
+    db_url, 
+    echo=False, 
+    future=True,
+    pool_pre_ping=True,  # Проверка соединения перед использованием
+    connect_args={
+        "options": "-c client_encoding=utf8"
+    })
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
 
